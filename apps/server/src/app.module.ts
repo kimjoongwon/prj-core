8db78dbc4aaad4775dec38f1ebcfd38682a2b2fc
kimjoongwon <kimjwally@gmail.com>
2024-02-18 14:43:17 +0900

    feat: 다시 정리

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,58 +1,21 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './modules/user/user.module';
 import { ClsModule } from 'nestjs-cls';
-import { ThrottlerModule } from '@nestjs/throttler';
-import { SharedModule } from './shared/shared.module';
-import { ApiConfigService } from './shared/services/api-config.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import {
-  AcceptLanguageResolver,
-  HeaderResolver,
-  I18nModule,
-  QueryResolver,
-} from 'nestjs-i18n';
-import path from 'path';
-import { HealthCheckerModule } from './modules/health-checker/health-checker.module';
 
 @Module({
   imports: [
-    UserModule,
     ClsModule.forRoot({
       global: true,
       middleware: {
         mount: true,
       },
     }),
-    ThrottlerModule.forRootAsync({
-      imports: [SharedModule],
-      useFactory: async (configService: ApiConfigService) => ({
-        throttlers: [configService.throttlerConfigs],
-      }),
-      inject: [ApiConfigService],
-    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
     PrismaModule,
-    I18nModule.forRootAsync({
-      imports: [SharedModule],
-      useFactory: async (configService: ApiConfigService) => ({
-        fallbackLanguage: configService.fallbackLanguage,
-        loaderOptions: {
-          path: path.join(__dirname, '/i18n/'),
-          watch: configService.isDevelopment,
-        },
-        resolvers: [
-          { use: QueryResolver, options: ['lang'] },
-          AcceptLanguageResolver,
-          new HeaderResolver(['x-lang']),
-        ],
-      }),
-      inject: [ApiConfigService],
-    }),
-    HealthCheckerModule,
   ],
   providers: [],
 })
--- apps/server/src/decorators/roles.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { Reflector } from '@nestjs/core';
-
-// eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export const Roles = Reflector.createDecorator<string[]>();
