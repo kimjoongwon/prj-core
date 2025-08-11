dacc05566b10c2a395fd5e7fe30e596bf25a2de8
kimjoongwon <kimjwally@gmail.com>
2025-08-11 11:26:00 +0900

    refactor: remove @nestjs/terminus dependency and update package configurations

--- packages/shared-schema/tsup.config.ts
+++ packages/shared-schema/tsup.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((option) => ({
-	entry: ["src/index.ts", "src/client.ts", "src/types.ts"],
-	format: ["cjs", "esm"],
-	dts: true,
-	clean: !option.watch,
-	splitting: !option.watch,
-	sourcemap: !!option.watch,
-	minify: !option.watch,
-	watch: option.watch,
-	external: ["@prisma/client"],
+  entry: ["src/index.ts", "src/client.ts", "src/types.ts"],
+  format: ["cjs", "esm"],
+  dts: true,
+  clean: !option.watch,
+  splitting: !option.watch,
+  sourcemap: !!option.watch,
+  minify: !option.watch,
+  watch: option.watch,
+  external: [
+    "@prisma/client",
+    "class-transformer/storage",
+    "@nestjs/websockets/socket-module",
+    "@nestjs/microservices",
+    "@nestjs/microservices/microservices-module",
+    "@nestjs/websockets",
+    "@nestjs/platform-socket.io",
+    "@nestjs/platform-ws",
+  ],
 }));
