587ba839165887be0f3138fe3db359e30bdbfa47
kimjoongwon <kimjwally@gmail.com>
2025-08-16 00:23:04 +0900

    refactor: remove PageProvider export from index file

--- packages/shared-frontend/src/provider/index.ts
+++ packages/shared-frontend/src/provider/index.ts
@@ -1,5 +1,4 @@
 export * from "./AppProviders";
 export * from "./AuthProvider/AuthProvider";
-export * from "./PageProvider";
 export * from "./QueryProvider/QueryProvider";
 export * from "./UIProviders";
